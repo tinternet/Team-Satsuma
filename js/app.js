ed9efc6e67cd9f9aaf282f544caee192780f3552
Alexandur Seferinkyn <d0ntth1nc@gmail.com>
2015-04-06 14:29:25 +0300

    added parse.com api

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 "use strict";
 (function( $, requirejs ) {
-// Basic configuration
 requirejs.config({
 	baseUrl: "js/"
 });
+
+Parse.initialize("sEqxBoQA1xw3ifFF8WBFttknx1QKuHHJfiipDPTG", "GRWQnDjiBY79Tt3RKUzjLZnG5hi2fpKapnLu4tFs");
+
+var TestObject = Parse.Object.extend("TestObject"),
+		testObject = new TestObject();
+		
+testObject
+	.save( { foo: "bar" } )
+	.then(function( object ) {
+		alert( "Test object saved to parse.com" );
+	});
 
 require( [ 'sampleModule' ], function( sampleModule ) {
 	sampleModule.show( "...AND BOOTSTRAP!" );
