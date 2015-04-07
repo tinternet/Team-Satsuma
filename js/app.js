4a735d89cf0e66e505fd3b6db0530bd542ee3b9f
Alexandur Seferinkyn <d0ntth1nc@gmail.com>
2015-04-07 09:28:07 +0300

    added some pointing comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 "use strict";
 (function( $, requirejs ) {
 	
+// Require js configuration
 requirejs.config({
 	baseUrl: "js/"
 });
 
+// Parse.com api initializer
 Parse.initialize( "sEqxBoQA1xw3ifFF8WBFttknx1QKuHHJfiipDPTG", "GRWQnDjiBY79Tt3RKUzjLZnG5hi2fpKapnLu4tFs" );
 
-require( [ "./authentication-system" ], function() {
+require( [
+	"./authentication-system"
+	
+	/* Add more *CORE* dependencies to this array...
+		There is no need to include modules for later use!
+		Note: We require a module *just* before we need it! */
+], function() {
+	// Main method
 	
 });
 
 }( $, requirejs ));
+
