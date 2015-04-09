aac4196e2d3674c70ddcceca87490d91155b7ffc
Julia Stambolieva <yuliya.f.s@gmail.com>
2015-04-09 10:59:26 +0300

    Click events - Forum / Menu / Submenu

--- js/controllers/show-forum-categories.js
+++ js/controllers/show-forum-categories.js
@@ -2,7 +2,10 @@
 define(function() {
 	var $forumCategories = $('.show-forum-categories'),
 		$imgPanelBody = $('.show-categories img'),
-		$panelBody = $('.show-categories ul');
+		$panelBody = $('.show-categories > ul'),
+		$submenu = $('.show-categories > ul > li > a'),
+		$submenuCategories = $('.show-categories > ul > li > ul');
+		//console.log($submenuCategories);
 		$panelBody.hide();
 		
 
@@ -13,4 +16,16 @@ $forumCategories.on("click", function(e)  {
 	$panelBody.show();
 	
 });
+
+$submenu.on("click", function(e) {
+	var $currCategory = $(e.target),
+		$currUl = $currCategory.next();
+		
+		if($currUl.is(':visible')) {
+			$currUl.hide();
+		} else {
+			$currUl.show()
+		}
+});
+
 });
