0c83111a7e68f2945cef81478a40be97c3661c01
nikydobrev <nikydobrev@gmail.com>
2015-04-11 20:46:52 +0300

    Sync with new account in Parse.com

--- js/parseDotComHeader.js
+++ js/parseDotComHeader.js
@@ -1,5 +1,5 @@
 "use strict";
 define({
-	"X-Parse-Application-Id": "sEqxBoQA1xw3ifFF8WBFttknx1QKuHHJfiipDPTG",
-	"X-Parse-REST-API-Key": "hZ5Tyo5pH5t3P65hO0a7iPZuxj2MS96BKtBUZkTB"
+	"X-Parse-Application-Id": "x7AxhOrmjVYWNby7K1a7JMMp00O8LMD5jomywrqm",
+	"X-Parse-REST-API-Key": "SS9j7hFeljU3uWCV757iWURI1KDhcyhsrtcozUkV"
 });
